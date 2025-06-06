0ff10084f9a4652207cf598d9e89b38ec3700d80
astha-gh <asthadevadiga7@gmail.com>
2025-06-06 15:59:01 +0530

    Remove dev.js from Git

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOURI: process.env.MONGOURI || "mongodb+srv://asthadevadiga7:2AGB431xBIPbBAVd@asthacluster.gvjdyip.mongodb.net/?retryWrites=true&w=majority&appName=asthaCluster",
-    JWT_SECRET: process.env.JWT_SECRET || "popipopipopoppippo"
-}
